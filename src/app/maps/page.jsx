c4abee764a128194c20178f462f0c5941eff064d
poone094 <83449058+poone094@users.noreply.github.com>
2023-08-03 00:55:24 +0700

    Update page.jsx

--- src/app/maps/page.jsx
+++ src/app/maps/page.jsx
@@ -3,10 +3,6 @@ import React from "react";
 import dynamic from "next/dynamic";
 import styles from "./page.module.css";
 
-export const metadata = {
-  title: "แผนที่สถานการณ์ชายฝั่ง",
-};
-
 export default function Maps() {
   const MapWithNoSSR = dynamic(() => import("@/components/Map/Map"), {
     ssr: false,
